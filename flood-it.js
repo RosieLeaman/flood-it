decf93e935a2f02de745f6fb450102e13f91e739
RosieLeaman <rosie.leaman@gmail.com>
2020-05-26 16:37:45 +0200

    added placeholder for instructions text and the header parts, and code to pick random colours from a selection

--- flood-it.js
+++ flood-it.js
@@ -1,5 +1,15 @@
 'use strict';
 
+function getNRandomColours(availableColours,n){
+  const numColours = availableColours.length
+
+  const colours = Array(n).fill(1).map((el) => {
+    return availableColours[Math.floor(Math.random()*numColours)]
+  })
+
+  return colours
+}
+
 function Button(props){
   return (
     <button
@@ -11,7 +21,23 @@ function Button(props){
   )
 }
 
-class Game extends React.Component {
+function InstructionText(){
+  return (
+    <p>These are instructions.</p>
+  )
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderSquare(key,colour){
+    
+  }
+}
+
+class Header extends React.Component {
   constructor(props) {
     super(props);
   }
@@ -20,7 +46,32 @@ class Game extends React.Component {
     return (
       <div className="header">
         <span className="game-name">Flood it!</span>
+        <Button
+          className="header-button"
+          text="New game"
+        />
+        <Button
+          className="header-button"
+          text="Instructions"
+        />
+        <InstructionText/>
       </div>
+    )
+  }
+}
+
+const availableColours = ['red','blue','green','yellow','orange']
+
+const board = getNRandomColours(availableColours,100)
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Header/>
     )
   }
 }
